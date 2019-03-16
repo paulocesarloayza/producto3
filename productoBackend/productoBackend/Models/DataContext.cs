b9d2c1ce0b83c77c6ac457a1045d0a09733d69ff
Paulo Cesar Loayza Carrasco <paulocesarloayza@upsa.edu.bo>
2019-03-16 11:18:43 -0400

    primer commit

---
+++ productoBackend/productoBackend/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace productoBackend.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<productoBackend.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ productoBackend/productoBackend/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿namespace productoBackend.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Producto
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name="Nombre del Producto")]
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }  
+    }
+}
